127f26e1a5a0871396b380b4ff0e2f8e5701bb98
yonikosiner <yoni@kosiner.co.uk>
2020-08-31 20:26:43 +0100

    install commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './styles/App.scss';
 // Logo
 import logo from './images/Logo.svg';
+// Menu
+import menu from './Components/menu.js';
+// Social icons
+import instagram from './images/instagram.svg';
+import twitter from './images/twitter.svg';
+import email from './images/email.svg';
+import git from './images/github.svg'
+// Image of me
+import me from './images/me.jpg'
 // Import footer
 import Footer from './Components/Footer.js';
 // Footer styling
@@ -10,11 +20,29 @@ import './styles/footer.scss';
 function App() {
   return (
     <div className="app">
+      <menu />
       {/* Logo */}
       <img className="logo" src={logo} width="139px" height="116px"></img>
+      {/* Image of me */}
+      <img src={me} class="me" width="458.5px" height="720px"></img>
       {/* Title */}
       <p className="hello">Hello my name is Yoni Kosiner</p>
       <h3 className="list">Developer, Designer,Student</h3>
+      {/* Social */}
+      <div className="social">
+        <a href="https://www.instagram.com/kosiner.codes" target=" _blank">
+          <img className="image" src={instagram}></img>
+        </a>
+        <a href="https://twitter.com/KosinerYoni" target=" _blank">
+          <img className="image1" src={twitter}></img>
+        </a>
+        <a href="mailto:yoni@kosiner.co.uk?subject=Yoni%20Kosiner%20Inquiry" target=" _blank">
+          <img className="image2" src={email}></img>
+        </a>
+        <a href="https://github.com/yonikosiner" target=" _blank">
+          <img className="image3" src={git}></img>
+        </a>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/Components/menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const menu = () => {
+    return (
+        <div className="app">
+            <div className="menu-wrapper">
+                <div className="hamburger-menu"></div>
+            </div>
+            <div className="inside">
+                <link>Home</link>
+                <link>About</link>
+                <link>Home automation</link>
+            </div>
+        </div>
+    )
+}
+
+export default menu;
