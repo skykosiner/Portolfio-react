fe19ff0f58793d69a25b4e1c4e2ae6fcd0bab9eb
yonikosiner <yoni@kosiner.co.uk>
2020-09-01 17:58:23 +0100

    trying to fix a issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles/App.scss';
 // Logo
 import logo from './images/Logo.svg';
 // Menu
-import menu from './Components/menu.js';
+import menu from './Components/Menu';
 // Menu scss
 import './styles/menu.scss';
 // Social icons
@@ -23,25 +23,25 @@ function App() {
     <div className="app">
       <menu />
       {/* Logo */}
-      <img className="logo" src={logo} width="139px" height="116px"></img>
+      <img alt="" className="logo" src={logo} width="139px" height="116px"></img>
       {/* Image of me */}
-      <img src={me} class="me" width="458.5px" height="720px"></img>
+      <img alt="" src={me} className="me" width="458.5px" height="720px"></img>
       {/* Title */}
       <p className="hello">Hello my name is Yoni Kosiner</p>
       <h3 className="list">Developer, Designer,Student</h3>
       {/* Social */}
       <div className="social">
         <a href="https://www.instagram.com/kosiner.codes" target=" _blank">
-          <img className="image" src={instagram}></img>
+          <img alt="" className="image" src={instagram}></img>
         </a>
         <a href="https://twitter.com/KosinerYoni" target=" _blank">
-          <img className="image1" src={twitter}></img>
+          <img alt="" className="image1" src={twitter}></img>
         </a>
         <a href="mailto:yoni@kosiner.co.uk?subject=Yoni%20Kosiner%20Inquiry" target=" _blank">
-          <img className="image2" src={email}></img>
+          <img alt="" className="image2" src={email}></img>
         </a>
         <a href="https://github.com/yonikosiner" target=" _blank">
-          <img className="image3" src={git}></img>
+          <img alt="" className="image3" src={git}></img>
         </a>
       </div>
       <Footer />
--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class menu extends Component {
-    click() {
-        alert("Im an alert");
-    }
-    render() {
-        return (
-            <div className="app">
-                <button onClick={this.click}><div className="menu-wrapper">
-                    <div className="hamburger-menu"></div>
-                </div>
-                </button>
-                <div className="inside">
-                    <a>Home</a>
-                    <a>About me</a>
-                    <a>Home automation </a>
-                </div>
+function menu() {
+    return (
+        <div className="app">
+            <div className="menu-wrapper">
+                <div className="hamburger-menu"></div>
             </div>
-        )
-    }
+            <div className="inside">
+                <a>Home</a>
+                <a>About me</a>
+                <a>Home automation </a>
+            </div>
+        </div>
+    );
 }
+
+export default menu;
