eff1bdb324de40446a8a09e0bd5ebfe3063921cf
yonikosiner <yoni@kosiner.co.uk>
2020-09-13 12:35:35 +0100

    starting link page probley

--- src/Links.js
+++ src/Links.js
@@ -5,10 +5,27 @@ import './styles/links.scss'
 import me from './images/me.jpg'
 
 function Links() {
+    function insta() {
+        alert("hello world")
+    }
+    function twitter() {
+        alert("hello world")
+    }
+    function portfolio() {
+        alert("hello world")
+    }
     return (
         <div className="app">
+            <h1 className="title">Links</h1>
+            {/* Center image */}
             <div className="center">
-                <img src={me} alt="" className="center_ me-img" />
+                {/* Acutal image */}
+                <img className="center_ img-me" src={me} alt="" />
+            </div>
+            <div className="links">
+                <button onClick={insta} className="instagram">📷  Instagram</button>
+                <button onClick={twitter} className="twitter"> 🐦 Twitter</button>
+                <button onClick={portfolio} className="portfolio">🧑‍💻 Portfolio</button>
             </div>
         </div >
     );
