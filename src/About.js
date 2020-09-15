2d0f36ae01b937dfd1e6ab9c34620d15be612604
yonikosiner <yoni@kosiner.co.uk>
2020-09-15 22:12:57 +0100

    fixed menu and about page

--- src/About.js
+++ src/About.js
@@ -4,6 +4,8 @@ import '/Users/yonikosiner/my-app/src/styles/About.scss';
 import './styles/App.scss';
 // Import menu
 import Menu from '/Users/yonikosiner/my-app/src/Components/Menu/Menu.component.jsx';
+// Logo
+import logo from './images/Logo.svg';
 // Import footer
 import Footer from './Components/Footer/Footer.component.jsx';
 
@@ -12,7 +14,16 @@ function About() {
         <div className="app">
             <Menu />
             <div id="all">
-                <h1>About</h1>
+                {/* Logo */}
+                <img className="logo" src={logo} alt="" width="139px" height="116px"></img>
+                {/* Title */}
+                <h1 className="title">About</h1>
+                {/* About text title */}
+                <p className="hello-world">Hello world 👋 my name is Yoni Kosiner or known
+                    <br /> online as <a className="global-link global-underline" href="https://www.instagram.com/kosiner.codes " target="_blank">@kosinercodes</a></p>
+                {/* About text */}
+                <p className="hello-text">I’m currently a student studying computer science. (self taught web dev/iOS/python before
+                <br /> learning computer science at school)</p>
                 <Footer />
             </div>
         </div >
