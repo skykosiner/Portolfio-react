f0fc09fc72e323c6196cccf7298008cf5b8256d2
yonikosiner <yoni@kosiner.co.uk>
2020-10-13 19:34:48 +0100

    install commit

--- src/Adminpage.js
+++ src/Adminpage.js
@@ -3,14 +3,17 @@ import fire from './config/Fire';
 import './styles/admin.scss'
 
 class admin extends Component {
+    // Log user out
     constructor(props) {
         super(props);
         this.logout = this.logout.bind(this);
     }
-
-
     logout() {
         fire.auth().signOut();
+    }
+    // New blog post
+    new() {
+
     }
 
     render() {
@@ -18,6 +21,8 @@ class admin extends Component {
             <div>
                 <h1>Welcome back Yoni</h1>
                 <div className="line"></div>
+                <div onClick={this.new} style={{ cursor: "pointer" }} className="new-post">Upload a new blog post
+                <br />+</div>
                 <div style={{ cursor: "pointer" }} onClick={this.logout}>Logout</div>
             </div>
         );
--- src/Blog.js
+++ src/Blog.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Log from './Components/Blog/blog.component.jsx';
-import Blogcard from './Components/Blog-card/blog-card.component.jsx';
 
 function blog() {
     return (
         <div className="app">
             <h1 className="title">Hello world welcome to my blog</h1>
-            <Blogcard />
+            <Log />
         </div>
     );
 }
