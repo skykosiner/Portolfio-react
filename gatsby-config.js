2b697a1f8491bc107718f5ee52342d18f53b4c73
yonikosiner <yoni@kosiner.co.uk>
2020-11-29 14:10:37 +0000

    my projects page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Yoni Kosiner portfolio`,
+    title: `Yoni Kosiner`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Yoni Kosiner`,
   },
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,123 +4,56 @@ import styled from 'styled-components';
 import lifeCover from "../images/lifeCover.png";
 import portfolio from "../images/portfolio.png";
 
-const ProjectsWrapper = styled.div`
-  font-family: "Roboto", sans-serif;
-`
-
-const Title = styled.h2`
-    font-size: 55px;
+const Title = styled.p`
+    font-weight: bold;
     text-align: center;
+    font-size: 69px;
 `
 
-// For all projects
-const Projects = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+const Projects = styled.div``
+
+
+const Project = styled.div``
+
+const ProjectTitle = styled.p`
+    font-size: 42px;
+    font-weight: bold;
+    margin-top: 10%;
+    margin-left: 10%;
+`
+const ProjectImg = styled.img`
+    position: absolute;
+    right: 3%;
+    top: 20%;
+`
+const Desc = styled.p`
+    margin-top: -2%;
+    font-size: 26px;
+    margin-left: 9.9%;
 `
 
-// For each signaller project
-const Project = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-    justify-content: center;
-    align-items: center;
-`
-
-// The project title
-const ProjectTitle = styled.h1`
-    text-align: center;
-`
-// Live text
-const SeeLive = styled.button`
-    border: 5px solid #333333;
-    background: none;
-    transition: 0.4s ease all;
-    font-size: 23px;
-    width: 20%;
-    &:hover{
-        background: #333333;
-        color: #fff;
-    }
-`
-// Button to see code
-const SeeCode = styled.button`
-   border: 5px solid #333333;
-    background: none;
-    transition: 0.4s ease all;
-    margin-top: 10px;
-    font-size: 23px;
-    width: 20%;
-    &:hover{
-        background: #333333;
-        color: #fff;
-    }    
-`
-// Image to show project
-const Image = styled.img`
-    width: 616px;
-    height: 311px;
-    object-fit: cover;
-`
-const More = styled.p`
-    font-size: 25px;
-    text-align: center;
-    margin-top: 40px;
-`
 
 const myProjects = () => {
-    // See life cover in trust live
-    const lifeLive = () => {
-        window.open(
-            'https://lifecoverintrust.com',
-            '_blank'
-        )
-    }
-    // See portfolio live
-    const portfolioLive = () => {
-        window.location.href = '/'
-    }
-    // See Life cover in trust code
-    const lifeCode = () => {
-        window.open(
-            'https://github.com/yonikosiner/Life-in-cover-trust',
-            '_blank'
-        )
-    }
-    // See code for portfolio
-    const portfolioCode = () => {
-        window.open(
-            'https://github.com/yonikosiner/Portolfio-react',
-            '_blank' 
-        );
-    }
     return (
         <Layout>
-            <ProjectsWrapper>
-            <Title>My Projects</Title>
+            <Title>Projects</Title>
             <Projects>
+                {/* Life cover in trust */}
                 <Project>
-                    <ProjectTitle>
-                        Life Cover In Trust
-                    </ProjectTitle>
-                    <Image src={lifeCover} alt="life cover in trust home" />
-                    <SeeLive onClick={lifeLive}>See live</SeeLive>
-                    <SeeCode onClick={lifeCode}>See code</SeeCode>
+                    <ProjectTitle>Life Cover In Trust</ProjectTitle>
+                    <ProjectImg src={lifeCover} alt="Life cover in trust home"/>
+                    <Desc>
+                        This is my first client project as a 
+                        <br />freelance web developer. This was built 
+                        <br />using Gatsby, express and styled
+                        <br />components.</Desc>
                 </Project>
-                <Project>
-                    <ProjectTitle>
-                        Portfolio
-                    </ProjectTitle>
-                    <Image src={portfolio} alt="portfolio home screen" />
-                    <SeeLive onClick={portfolioLive}>See live</SeeLive>
-                    <SeeCode onClick={portfolioCode}>See code</SeeCode>
-                </Project>
+                {/* Portfolio */}
+                <project>
+                    <ProjectTitle>Portfolio</ProjectTitle>
+                    <ProjectImg src={portfolio} alt="Portfolio home" />
+                </project>
             </Projects>
-            {/* More to come */}
-            <More>More to come ; )</More>
-           </ProjectsWrapper>
         </Layout>
     );
 }
